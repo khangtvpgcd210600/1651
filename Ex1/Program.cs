7c5442d5a81d1d3b150e8dce8fb870dc65734a01
kentuky2010 <phuckhang01@gmail.com>
2023-01-09 16:04:54 +0700

    Add project files.

---
+++ Ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dice roll = new Dice();
+
+        Console.Write("Enter the number of sides: ");
+        int side = int.Parse(Console.ReadLine());
+        roll.Side = side;
+        Console.WriteLine("The number is: " + roll.Roll());
+    }
+}
---
+++ Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex2;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Student student1 = new Student();
+        student1.Name = "Khang";
+        student1.Age = 21;
+        student1.Gpa = 2.5f;
+        student1.DisplayInfomation();
+        Console.WriteLine();
+        Student student2 = new Student(21, 5f, "Khang Prime");
+        student2.DisplayInfomation();
+    }
+}
