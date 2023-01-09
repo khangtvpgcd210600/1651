ad20bee10c4eb7ee6b319ddaf06dd4f3707679dc
kentuky2010 <phuckhang01@gmail.com>
2023-01-09 16:23:17 +0700

    Add solution

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -9,6 +9,29 @@ class Program
         Console.Write("Enter the number of sides: ");
         int side = int.Parse(Console.ReadLine());
         roll.Side = side;
-        Console.WriteLine("The number is: " + roll.Roll());
+        int trueNumber = roll.Roll();
+        while (true)
+        {
+            Console.WriteLine("Please guess a number");
+            int guessNumber = int.Parse(Console.ReadLine());
+            if (guessNumber == trueNumber)
+            {
+                Console.WriteLine("You've guess the right number!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("You've guess the wrong number!");
+                Console.WriteLine("Press n to end the program or you can try again!");
+                char character = char.Parse(Console.ReadLine());
+                if (character == 'n')
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+        }
+
+
     }
 }
