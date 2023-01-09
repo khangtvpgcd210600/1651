a57f14a8c7e6f62ed1264487d8b8ea777105aa28
kentuky2010 <phuckhang01@gmail.com>
2023-01-09 22:25:48 +0700

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,105 @@
+﻿using Ex3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Exercise1();
+        Exercise2();
+        Exercise3();
+        Exercise4();
+
+    }
+    public static void Exercise1()
+    {
+        Dice roll = new Dice();
+
+        Console.Write("Enter the number of sides: ");
+        int side = int.Parse(Console.ReadLine());
+        roll.Side = side;
+        int trueNumber = roll.Roll();
+        while (true)
+        {
+            Console.WriteLine("Please guess a number");
+            int guessNumber = int.Parse(Console.ReadLine());
+            if (guessNumber == trueNumber)
+            {
+                Console.WriteLine("You've guess the right number!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("You've guess the wrong number!");
+                Console.WriteLine("Press n to end the program or you can try again!");
+                char character = char.Parse(Console.ReadLine());
+                if (character == 'n')
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void Exercise2()
+    {
+        Person person = new Person();
+        person.Name = "Test";
+        person.Age = 1;
+        person.DisplayPerson();
+
+        Student student = new Student();
+        student.Name = person.Name;
+        student.Age = person.Age;
+        student.Gpa = 3.0f;
+        student.DisplayStudent();
+    }
+
+    public static void Exercise3()
+    {
+        int numberofSongs = int.Parse(Console.ReadLine());
+        Song[] songs = new Song[numberofSongs];
+        for (int i = 0; i < numberofSongs; i++)
+        {
+            string[] data = Console.ReadLine().Split("_");
+            songs[i] = new Song();
+            songs[i].TypeList = data[0];
+            songs[i].Name = data[1];
+            songs[i].Time = data[2];
+        }
+        string typeList = Console.ReadLine();
+        if (typeList == "all")
+        {
+            foreach (Song song in songs)
+            {
+                Console.WriteLine(song.Name);
+            }
+        }
+        else
+        {
+            foreach (Song song in songs)
+            {
+                if (song.TypeList == typeList)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+        }
+    }
+
+    public static void Exercise4()
+    {
+        Fraction fraction1 = new Fraction(int.Parse(Console.ReadLine()),
+            int.Parse(Console.ReadLine()));
+        Fraction fraction2 = new Fraction(int.Parse(Console.ReadLine()),
+            int.Parse(Console.ReadLine()));
+        Console.Write("Add: "); fraction1.Add(fraction2);
+        Console.Write("Subtract: "); fraction1.Subtract(fraction2);
+        Console.Write("Mulitply: "); fraction1.Multiply(fraction2);
+        Console.Write("Divide: "); fraction1.Divide(fraction2);
+        Console.Write("Fraction 1: "); fraction1.DisplayFraction();
+        Console.Write("Fraction 2: "); fraction2.DisplayFraction();
+        Console.Write("Fraction 1 in decimal: "); fraction1.DisplayFractionInDecimal();
+        Console.Write("Fraction 2 in decimal: "); fraction2.DisplayFractionInDecimal();
+    }
+}
---
+++ Ex3/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex3
+{
+    internal class Student : Person
+    {
+        private float gpa;
+
+        public float Gpa { get { return gpa; } set { gpa = value; } }
+
+        public Student() { }
+        public Student(string name, int age, float gpa)
+        {
+            Name = name;
+            Age = age;
+            Gpa = gpa;
+        }
+
+        public void DisplayStudent()
+        {
+            Console.WriteLine($"Name: {Name}\nAge: {Age}\nGPA: {Gpa}");
+        }
+
+        public void Input() { }
+    }
+}
