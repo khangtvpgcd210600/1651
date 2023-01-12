6f13a9b1522319c6f3eaf9ec225648607c02337f
kentuky2010 <phuckhang01@gmail.com>
2023-01-12 16:11:41 +0700

    Add Lab 1

---
+++ Lab1/Ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ex1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dice roll = new Dice();
+
+        Console.Write("Enter the number of sides: ");
+        int side = int.Parse(Console.ReadLine());
+        roll.Side = side;
+        int trueNumber = roll.Roll();
+        while (true)
+        {
+            Console.WriteLine("Please guess a number");
+            int guessNumber = int.Parse(Console.ReadLine());
+            if (guessNumber == trueNumber)
+            {
+                Console.WriteLine("You've guess the right number!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("You've guess the wrong number!");
+                Console.WriteLine("Press n to end the program or you can try again!");
+                char character = char.Parse(Console.ReadLine());
+                if (character == 'n')
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
