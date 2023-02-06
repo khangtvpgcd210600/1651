790bbd7cc99e65c1af8befd7558144d55a3c2c85
kentuky2010 <phuckhang01@gmail.com>
2023-02-06 15:43:51 +0700

    Demo Lab5 final change

--- Lab5/Ex1/Program.cs
+++ Lab5/Ex1/Program.cs
@@ -4,12 +4,16 @@
     {
         public static void Main(string[] args)
         {
-            Cat cat1 = new Cat();
+            Cat cat1 = new Cat("Cat");
 
             cat1.Eat();
             cat1.Run();
             cat1.Jump();
             cat1.Climb();
+
+            Cow cow1 = new Cow("Cow");
+            cow1.Eat();
+            cow1.Run();
         }
     }
 }
